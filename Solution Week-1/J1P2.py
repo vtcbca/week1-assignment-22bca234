e14d8754cb47e577a7766ea4ecdd3c7dc68111c7
22bca234 <127925445+22bca234@users.noreply.github.com>
2023-06-29 22:11:42 +0530

    Program 2.py

--- Solution Week-1/J1P2.py
+++ Solution Week-1/J1P2.py
@@ -1,8 +1,10 @@
 #2.Write a python script to enter any number and print the sum of its digit.
-def sum():
-    n=int(input("Enter a number : "))
-    s=0
-    for i in range(n+1):
-        s=s+i
-    print(s)
-sum()
+def digsum():
+    n=int(input('Enter a number whose sum you want to print : '))
+    sum=0
+    while (n>0):
+        a=n%10
+        sum=sum+a
+        n=n//10
+    print(sum)
+digsum()
